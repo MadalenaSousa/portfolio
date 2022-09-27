16de8bdec267a8cf521b34432c4e3109366773cd
unknown <madalenaantunessousa@gmail.com>
2022-09-27 16:10:03 +0100

    Work page and tried to fix some bugs

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
-let inc = 50;
+let inc = -50;
 let i = 0;
-let works = document.querySelectorAll(".inner div");
+let works = document.querySelectorAll(".inner .work");
 
 document.onkeydown = (e) => {
   e = e || window.event;
@@ -8,12 +8,16 @@ document.onkeydown = (e) => {
   if (e.keyCode === 37)
   {
     if(i != 0) {
-      inc = inc + 50;
+      inc = inc + 100;
       i--;
+
+      for(let j = 0; j < works.length; j++) {
+          works[j].style.webkitTransform = "translateX(" + inc + "%) scale(1)";
+      }
 
       document.getElementById('highlight').removeAttribute('id');
       works[i].id = 'highlight';
-      document.querySelector(".inner").style.webkitTransform = "translateX(" + inc + "%)";
+      works[i].style.webkitTransform = "translateX(" + inc + "%) scale(1.25)";
 
       console.log('Left arrow pressed, New Transform: ' + document.querySelector(".inner").style.transform + ' Applied Inc:' + inc);
     }
@@ -21,12 +25,16 @@ document.onkeydown = (e) => {
   else if (e.keyCode === 39)
   {
     if(i != works.length-1) {
-      inc = inc - 50;
+      inc = inc - 100;
       i++;
+
+      for(let j = 0; j < works.length; j++) {
+          works[j].style.webkitTransform = "translateX(" + inc + "%) scale(1)";
+      }
 
       document.getElementById('highlight').removeAttribute('id');
       works[i].id = 'highlight';
-      document.querySelector(".inner").style.webkitTransform = "translateX(" + inc + "%)";
+      works[i].style.webkitTransform = "translateX(" + inc + "%) scale(1.25)";
 
       console.log('Right arrow pressed, New Transform: ' + document.querySelector(".inner").style.transform + ' Applied Inc:' + inc);
     }
