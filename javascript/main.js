41efd4250d55a4a410fa1e43cb9d1ef25d1e24bc
unknown <madalenaantunessousa@gmail.com>
2022-09-30 17:18:54 +0100

    Json file connection

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,19 @@
 let inc = -50;
 let i = 0;
-let works = document.querySelectorAll(".inner .work");
+let row = document.querySelector(".outter");
+let works;
+
+fetch('./works.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (json) {
+        //console.log(json);
+
+        row.appendChild(createWork(json));
+
+        works = document.querySelectorAll(".inner .work");
+    });
 
 document.onkeydown = (e) => {
   e = e || window.event;
@@ -39,4 +52,39 @@ document.onkeydown = (e) => {
       console.log('Right arrow pressed, New Transform: ' + document.querySelector(".inner").style.transform + ' Applied Inc:' + inc);
     }
   }
+}
+
+function createWork(json) {
+
+  let workColumn = document.createElement("div");
+  workColumn.classList.add("twelve");
+  workColumn.classList.add("columns");
+  workColumn.classList.add("inner");
+
+  for(let i = 0; i < json.length; i++) {
+    let divBox = document.createElement("div");
+    divBox.classList.add("work");
+
+    if(i == 0) {
+        divBox.id = "highlight";
+    }
+
+    let workLink = document.createElement("a");
+    workLink.setAttribute("src", json[i].link);
+
+    let smallDivBox = document.createElement("div");
+
+    let workImg = document.createElement("img");
+    workImg.setAttribute("width", "100%");
+    workImg.setAttribute("src", json[i].cover);
+    workImg.setAttribute("alt", "");
+
+    smallDivBox.appendChild(workImg);
+    workLink.appendChild(smallDivBox);
+    divBox.appendChild(workLink);
+
+    workColumn.appendChild(divBox);
+  }
+
+  return workColumn;
 }
