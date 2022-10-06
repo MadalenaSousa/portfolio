39b6acfd0fc55dda528ea2e1aec4a97c9c5d6873
unknown <madalenaantunessousa@gmail.com>
2022-10-06 18:27:37 +0100

    Button hover

--- javascript/main.js
+++ javascript/main.js
@@ -5,3 +5,31 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+let navItens = document.querySelectorAll(".nav-item");
+let navSvg = document.querySelectorAll(".navHover");
+
+for(let i = 0; i < navItens.length; i++) {
+  navItens[i].addEventListener("mouseenter", function () {
+    navSvg[i].classList.remove("displayNone");
+    navSvg[i].classList.add("hovereffect");
+  });
+
+  navItens[i].addEventListener("mouseleave", function () {
+    navSvg[i].classList.add("displayNone");
+    navSvg[i].classList.remove("hovereffect");
+  });
+}
+
+let sendButton = document.querySelector(".buttonwithhover");
+let contactSvg = document.querySelector(".contactHover");
+
+sendButton.addEventListener("mouseenter", function () {
+  contactSvg.classList.remove("displayNone");
+  contactSvg.classList.add("hovereffect");
+});
+
+sendButton.addEventListener("mouseleave", function () {
+  contactSvg.classList.add("displayNone");
+  contactSvg.classList.remove("hovereffect");
+});
