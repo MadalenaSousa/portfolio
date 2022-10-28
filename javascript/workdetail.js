2ba5c6cfe38a9b7697b56298a59256a129bfaefa
unknown <madalenaantunessousa@gmail.com>
2022-10-28 15:00:47 +0100

    Changed Line Arts

--- javascript/workdetail.js
+++ javascript/workdetail.js
@@ -118,3 +118,22 @@ function createWorkDetail(workObject) {
         workDetailBox.appendChild(midSpace);
     }
 }
+
+let topButton = document.querySelector(".top-button");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+topButton.addEventListener("click", topFunction);
